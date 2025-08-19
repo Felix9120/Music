bb4c3b7da7c202e08d5ed6a3ddd7c94cbdd7dc3d
Felix9120 <felixbarrios9120@hotmail.com>
2025-08-18 21:22:47 -0500

    actualizar

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,48 +1,30 @@
-// src/componentes/Sidebar.jsx
 import React, { useState } from 'react';
 import Home from '../assets/Home_Fill_S.png';
-import Logo from '../assets/logo.jpeg';
+
+import Logo from '../assets/logo.jpeg'
 import { Link } from 'react-router-dom';
 
 function Sidebar() {
-  const [query, setQuery] = useState("");
+   
 
-  const handleSearch = (e) => {
-    e.preventDefault();
+    return (
+        <div className="text-white rounded-[10px] w-full flex-col justify-between p-5 bg-black relative">
+            <div className='flex gap-2.5 items-center'>
 
-    // Guardamos búsqueda en localStorage
-    localStorage.setItem("search_query", query);
+                <div className='w-[80px] h-[80px]  flex items-center justify-center cursor-pointer'>   
+                     <img src={Logo} alt="" />
+                </div>           
 
-    // Disparamos evento personalizado
-    window.dispatchEvent(new Event("search-updated"));
-  };
 
-  return (
-    <div className="text-white rounded-[10px] w-full flex-col justify-between p-5 bg-black relative">
-      <div className="flex gap-2.5 items-center">
-        <div className="w-[80px] h-[80px] flex items-center justify-center cursor-pointer">
-          <img src={Logo} alt="Logo" />
+                <div className='bg-[#191919] w-[50px] h-[50px] rounded-[10px] flex items-center justify-center cursor-pointer'>         
+                   <Link to={'/Home'}>
+                        <img src={Home} alt="Home" />
+                    </Link>
+                </div>
+                
+            </div>
         </div>
-
-        <div className="bg-[#191919] w-[50px] h-[50px] rounded-[10px] flex items-center justify-center cursor-pointer">
-          <Link to={'/Home'}>
-            <img src={Home} alt="Home" />
-          </Link>
-        </div>
-      </div>
-
-      {/* 🔹 Buscador */}
-      <form onSubmit={handleSearch} className="mt-5">
-        <input
-          type="text"
-          placeholder="Buscar canciones..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          className="w-full p-2 rounded-md bg-[#191919] text-white focus:outline-none"
-        />
-      </form>
-    </div>
-  );
+    );
 }
 
 export default Sidebar;
